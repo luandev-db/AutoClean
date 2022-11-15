88960e061941872a6c1e2554b58e583ad49bb39f
luandev-ux <luan.videira@gmail.com>
2022-11-15 10:53:56 -0300

    luan

--- AutoClean.App/ViewModels/ToggleSwitchViewModel.cs
+++ AutoClean.App/ViewModels/ToggleSwitchViewModel.cs
@@ -1,61 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Runtime;
 using System.Threading.Tasks;
 using System.Windows;
-using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Controls;
+using AutoClean.App.ViewModels;
 using Microsoft.Win32;
 using Wpf.Ui.Common;
 using Wpf.Ui.Controls;
 using Wpf.Ui.Controls.Interfaces;
 using Wpf.Ui.Mvvm.Contracts;
+using AutoClean.App.Services;
+using System.IO;
 
-namespace AutoClean.App.ViewModels
+namespace AutoClean.App.Views.Pages;
+
+/// <summary>
+/// Interaction logic for Input.xaml
+/// </summary>
+public partial class ToggleSwitchViewModel
 {
-    public class ToggleSwitchViewModel : ObservableObject
+    private readonly ISnackbarService _snackbarService;
+
+
+
+    public ToggleSwitchViewModel(ISnackbarService snackbarService)
     {
-#pragma warning disable CS0649 // Campo "ToggleSwitchViewModel._snackbarService" nunca é atribuído e sempre terá seu valor padrão null
-        private readonly ISnackbarService _snackbarService;
-#pragma warning restore CS0649 // Campo "ToggleSwitchViewModel._snackbarService" nunca é atribuído e sempre terá seu valor padrão null
 
-#pragma warning disable CS0169 // O campo "ToggleSwitchViewModel._dialogControl" nunca é usado
-        private readonly IDialogControl _dialogControl;
-#pragma warning restore CS0169 // O campo "ToggleSwitchViewModel._dialogControl" nunca é usado
+        _snackbarService = snackbarService;
+    }
+    public void DisableCortana(object sender, RoutedEventArgs e)
+    {
 
+        ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+        string checkState;
 
-
-        public void ToggleSwitch(object sender, RoutedEventArgs e)
+        if (toggleSwitch.IsChecked == true)
         {
-            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
-            string checkState;
-            var Search = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Search", true);
-            var WindowsSearch = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\Windows Search", true);
+            checkState = "Cortana Desligada";
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\SearchSettings", "IsDeviceSearchHistoryEnabled", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "AllowCortana", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "DisableWebSearch", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "ConnectedSearchUseWeb", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "ConnectedSearchUseWebOverMeteredConnections", "0", RegistryValueKind.DWord);
 
-            if (!toggleSwitch.IsChecked.HasValue)
-                checkState = "Indeterminate";
-            else
-            {
-                if (toggleSwitch.IsChecked == true)
-                {
-                    checkState = "Cortana Desativada";
-                    Search.SetValue("AllowCortana", 0);
-                    Search.SetValue("BingSearchEnabled", 0);
-                    Search.SetValue("CortanaConsent", 0);
-                    WindowsSearch.SetValue("AllowCloudSearch", 0);
-                }
-                else
-                {
-                    checkState = "Cortana Ativada";
-                    Search.SetValue("AllowCortana", 1);
-                    Search.SetValue("BingSearchEnabled", 1);
-                    Search.SetValue("CortanaConsent", 1);
+            Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search", "HistoryViewEnabled", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search", "DeviceHistoryEnabled", "0", RegistryValueKind.DWord);
+
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "AllowSearchToUseLocation", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "BingSearchEnabled", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "CortanaConsent", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "AllowCloudSearch", "0", RegistryValueKind.DWord);
 
 
-                }
-
-                _snackbarService.Show(string.Format(checkState, SymbolRegular.FoodCake24, ControlAppearance.Primary));
-            }
         }
+        else
+        {
+            checkState = "Cortana Ativada";
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "AllowCortana", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "DisableWebSearch", "0", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "ConnectedSearchUseWeb", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "ConnectedSearchUseWebOverMeteredConnections", "1", RegistryValueKind.DWord);
+
+            Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search", "HistoryViewEnabled", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search", "DeviceHistoryEnabled", "1", RegistryValueKind.DWord);
+
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "AllowSearchToUseLocation", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "BingSearchEnabled", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search", "CortanaConsent", "1", RegistryValueKind.DWord);
+            Registry.SetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search", "AllowCloudSearch", "1", RegistryValueKind.DWord);
+
+
+        }
+
+        _snackbarService.Show(string.Format(checkState, SymbolRegular.FoodCake24, ControlAppearance.Primary));
+
+    }
+
+
+
+    public void ToggleSwitch_Checked(object sender, RoutedEventArgs e)
+    {
+
     }
 }
--- AutoClean.App/Views/Pages/WindowsPage.xaml.cs
+++ AutoClean.App/Views/Pages/WindowsPage.xaml.cs
@@ -18,6 +18,7 @@ using Wpf.Ui.Controls.Interfaces;
 using Wpf.Ui.Mvvm.Contracts;
 using AutoClean.App.Services;
 using System.IO;
+using Wpf.Ui.Common.Interfaces;
 
 namespace AutoClean.App.Views.Pages;
 
@@ -26,60 +27,25 @@ namespace AutoClean.App.Views.Pages;
 /// </summary>
 public partial class WindowsPage
 {
-    private readonly ISnackbarService _snackbarService;
-
-    private readonly IDialogControl _dialogControl;
-
-
-    public WindowsPage(ISnackbarService snackbarService, IDialogService dialogService)
+    public WindowsPage ViewModel
     {
-        InitializeComponent();
-
-        _snackbarService = snackbarService;
-        _dialogControl = dialogService.GetDialogControl();
+        get;
     }
+
+
     public void DisableCortana(object sender, RoutedEventArgs e)
     {
+
         
-        ToggleSwitch toggleSwitch = sender as ToggleSwitch;
-        string checkState;
-        var Search = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Search", true);
-        var WindowsSearch = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Policies\\Microsoft\\Windows\\Windows Search", true);
+        
+       
 
-
-        if (toggleSwitch.IsChecked == true)
-        {
-            checkState = "Cortana Desativada";
-            Search.SetValue("AllowCortana", 0);
-            Search.SetValue("BingSearchEnabled", 0);
-            Search.SetValue("CortanaConsent", 0);
-            WindowsSearch.SetValue("AllowCloudSearch", 0);
-           
-            
-        }
-        else
-        {
-            checkState = "Cortana Ativada";
-            Search.SetValue("AllowCortana", 1);
-            Search.SetValue("BingSearchEnabled", 1);
-            Search.SetValue("CortanaConsent", 1);
-            WindowsSearch.SetValue("AllowCloudSearch", 1);
-          
-
-        }
-
-        _snackbarService.Show(string.Format(checkState, SymbolRegular.FoodCake24, ControlAppearance.Success));
     }
 
 
 
     public void ToggleSwitch_Checked(object sender, RoutedEventArgs e)
     {
-       
-        
-        
-    }
-}
-       
 
-     
+    }
+}    
